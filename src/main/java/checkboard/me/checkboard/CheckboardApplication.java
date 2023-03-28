bafa54d59839f492d993854d269ef076f46f0bdc
内藤祐馬 <naitoyuma@outlook.jp>
2023-03-28 10:35:13 +0900

    init

---
+++ src/main/java/checkboard/me/checkboard/CheckboardApplication.java
@@ -0,0 +1,13 @@
+package checkboard.me.checkboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/checkboard/me/checkboard/CheckboardApplicationTests.java
@@ -0,0 +1,13 @@
+package checkboard.me.checkboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
