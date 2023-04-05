876e751564b1aa12c3e14935cd1c1e333c59998e
naitoyuma7110 <naitoyuma1118@gmail.com>
2023-04-05 17:31:50 +0900

    [add]docs作成

--- src/main/java/checkboard/me/checkboard/domain/model/UserCommentRepository.java
+++ src/main/java/checkboard/me/checkboard/domain/model/UserCommentRepository.java
@@ -3,8 +3,9 @@ package checkboard.me.checkboard.domain.model;
 import checkboard.me.checkboard.domain.type.UserComments;
 /*
     Repository：永続化の抽象
-    データを永続化させるためにはドメイン層とアプリケーション層、データのやり取りにDTOを使用する
-    Repositoryはその際の処理の抽象でありInterfaceとして定義される→永続化処理の仕様
+    データを永続化させるためにはドメイン層とアプリケーション層でDTOを使用したデータのやり取りを行う
+    Repositoryはその際の処理の抽象でありInterfaceとして定義される(永続化処理の抽象)
+
 */
 
 public interface UserCommentRepository {
