9591ce295d379b8741476d6e6c57b0719c42c23b
naitoyuma7110 <naitoyuma1118@gmail.com>
2023-04-07 13:03:35 +0900

    [fix]認証用テーブルの作成、H2-console操作設定と管理ユーザー作成のスキーマ

--- src/main/java/checkboard/me/checkboard/config/SecurityConfig.java
+++ src/main/java/checkboard/me/checkboard/config/SecurityConfig.java
@@ -14,7 +14,11 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().permitAll();
+//      H2が提供する"/h2-console/"から始まる管理画面のエンドポイントに対してはcsrf対策をせず、全てのアクセスを許可する
+//      frameOptions().sameOrigin():はsame-originであればiframeを許可している
+        http.authorizeRequests().antMatchers("/h2-console/**").permitAll()
+                .and().csrf().ignoringAntMatchers("/h2-console/**")
+                .and().headers().frameOptions().sameOrigin();
         return http.build();
     }
 }
